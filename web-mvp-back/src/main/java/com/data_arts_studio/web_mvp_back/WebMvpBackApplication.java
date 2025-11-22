19224f0d72a4b41134db96dae833cbdaa617da90
teauk <kto031124@gmail.com>
2025-11-22 21:12:17 +0900

    Spring Boot 프로젝트 초기 설정 추가

---
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/WebMvpBackApplication.java
@@ -0,0 +1,13 @@
+package com.data_arts_studio.web_mvp_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvpBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvpBackApplication.class, args);
+	}
+
+}
---
+++ web-mvp-back/src/test/java/com/data_arts_studio/web_mvp_back/WebMvpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data_arts_studio.web_mvp_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
