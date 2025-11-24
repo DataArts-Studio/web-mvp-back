3353882176416b7043f049cd44d26a63fc0fd332
teauk <kto031124@gmail.com>
2025-11-24 09:33:45 +0900

    refactor: Identifier를 UUID String 기반으로 전환

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/shared/Identifier.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/shared/Identifier.java
@@ -4,20 +4,21 @@ import java.util.UUID;
 
 // UUID 기반 Id VO abstract class
 public abstract class Identifier {
-    private final Long id;  
 
-    protected Identifier(Long id) {
+    private final String id;  
+
+    protected Identifier(String id) {
         this.id = id;
     }
 
     // UUID 생성 유틸리티 메서드
     // Entitiy의 ID를 값 객체로 생성
-    public static String newId(String id) {
+    public static String newId() {
         // 랜덤 UUID 생성
         return UUID.randomUUID().toString();
     }
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
     
