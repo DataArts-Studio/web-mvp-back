95f2d0a501b19a1478734e56dad1958bb71132fb
teauk <kto031124@gmail.com>
2025-12-01 14:53:04 +0900

    feat: TestCaseId 식별자 클래스 추가

---
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/testcase/domain/TestCaseId.java
@@ -0,0 +1,15 @@
+package com.data_arts_studio.web_mvp_back.testcase.domain;
+
+import com.data_arts_studio.web_mvp_back.shared.Identifier;
+
+public class TestCaseId extends Identifier {
+
+    public TestCaseId(String id) {
+        super(id);
+    }
+
+    public static TestCaseId create() {
+        return new TestCaseId(Identifier.newId());
+    }
+    
+}
