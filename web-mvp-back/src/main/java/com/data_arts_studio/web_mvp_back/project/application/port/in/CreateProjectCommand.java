7674290ed155db2d8afccb15ee37b04bec4598bb
teauk <kto031124@gmail.com>
2025-12-03 22:40:18 +0900

    feat: 프로젝트 생성용 입력 커맨드 객체 추가
    - name, description, ownerName 필드를 포함한 생성 요청 커맨드 객체 정의
    - record로 불변 구조 적용

---
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/application/port/in/CreateProjectCommand.java
@@ -0,0 +1,13 @@
+package com.data_arts_studio.web_mvp_back.project.application.port.in;
+
+// 프로젝트 생성 요청 시에 필요한 커맨드 객체 (입력)
+public record CreateProjectCommand(
+    String name,
+    // boolean isPravateMode, // 프라이빗 여부
+    // String password, // 프라이빗 모드일 경우 비밀번호
+    // String passwordConfirm,// 프라이빗 모드일 경우 비밀번호 확인
+    String description,
+    String ownerName
+) {
+    
+}
