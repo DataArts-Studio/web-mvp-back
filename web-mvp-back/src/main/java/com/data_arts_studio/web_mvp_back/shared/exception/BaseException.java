678790ad9dd224da303a8324ca607167f35ad7cf
teauk <kto031124@gmail.com>
2025-12-06 14:06:30 +0900

    refactor: 예외 상태 코드를 int에서 HttpStatus로 변경

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/shared/exception/BaseException.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/shared/exception/BaseException.java
@@ -1,21 +1,28 @@
 package com.data_arts_studio.web_mvp_back.shared.exception;
 
+import org.springframework.http.HttpStatus;
+
 public abstract class BaseException extends RuntimeException {
     private final String errorCode; // 에러 코드
-    private final int status; // HTTP 상태 코드
-
-    protected BaseException(String errorCode, int status, String message) {
+    private final HttpStatus httpStatus; // HTTP 상태 코드
+    
+    /**
+     * @param message 사용자 친화적인 메세지
+     * @param errorCode 내부에서 정의한 에러 코드
+     * @param httpStatus 클라이언트에 반환될 HTTP 상태 코드
+     */
+    protected BaseException(String message, String errorCode, HttpStatus httpStatus) {
         super(message);
         this.errorCode = errorCode;
-        this.status = status;
+        this.httpStatus = httpStatus;
     }
 
     public String getErrorCode() {
         return errorCode;
     }
 
-    public int getStatus() {
-        return status;
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
     }
 
     
