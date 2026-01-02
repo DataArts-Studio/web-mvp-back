d667a39d65910709fec3cf4f5403fc74078887d8
teauk <kto031124@gmail.com>
2026-01-02 16:31:47 +0900

    feat: Project에 JPA 복원용 생성자 및 아카이브 상태 처리 추가
    - JPA 복원용 생성자 추가
    - archive 메서드 및 ProjectStatus 관리 추가

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/domain/Project.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/domain/Project.java
@@ -1,56 +1,60 @@
 package com.data_arts_studio.web_mvp_back.project.domain;
 
+import java.time.LocalDateTime;
 import com.data_arts_studio.web_mvp_back.shared.BaseEntity;
+import lombok.Getter;
 
+@Getter
 public class Project extends BaseEntity {
     // 프로젝트 도메인 모델 스켈레톤
     private final ProjectId id; // 프로젝트 식별자
-    // private boolean isPravateMode; // 프라이빗 여부
     private String name; // 프로젝트 이름
     private String password; //프로젝트 비밀번호 
     private String description; // 프로젝트 설명
     private String ownerName; // 프로젝트 소유자 이름
+    private ProjectStatus status = ProjectStatus.ACTIVE; // 프로젝트 상태
 
-    public Project(ProjectId id, String name, String password, String description, String ownerName) {  //boolean isPravateMode, 
+
+    // 프로젝트 생성용 생성자
+    public Project(ProjectId id, String name, String password, String description, String ownerName, ProjectStatus status) { 
         super();
-        // this.isPravateMode = isPravateMode;
         this.id = id;
         this.name = name;
         this.password = password;
         this.description = description;
         this.ownerName = ownerName;
+        this.status = status;
     }
-
-    public ProjectId getId() {
-        return id;
+    // JPA 리스톨용 생성자
+    public Project(ProjectId id, String name,  String password, String description, String ownerName, ProjectStatus status, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) { 
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.description = description;
+        this.ownerName = ownerName;
+        this.status = status;
+        this.restoreAuditFields(createdAt, updatedAt, deletedAt);
+        
     }
-    public String getName() {
-        return name;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public String getDescription() {
-        return description;
-    }
-
-    public String getOwnerName() {
-        return ownerName;
-    }
-
+    // 프로젝트 수정
     public void rename (String newName) {
         this.name = newName;
         markUpdated();
     }
-
+    // 비밀번호 변경
     public void changePassword (String newPassword) {
         this.password = newPassword;
         markUpdated();
     }
-
+    // 설명 변경
     public void updateDescription (String newDescription) {
         this.description = newDescription;
         markUpdated();
+    }
+    // 프로젝트 아카이브
+    public void archive(String confirmName) {
+        this.status = ProjectStatus.ARCHIVED;
+        this.markUpdated();
     }
 
 }
