b997c5e596fbcd94509ffc5ce1c0582d750d736d
teauk <kto031124@gmail.com>
2026-01-03 21:59:57 +0900

    feat: Project domain Entity, ProjectJpaEntity 양방향 매핑용 ProjectMapper 구현

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/adapter/out/persistence/mapper/ProjectMapper.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/adapter/out/persistence/mapper/ProjectMapper.java
@@ -2,9 +2,35 @@ package com.data_arts_studio.web_mvp_back.project.adapter.out.persistence.mapper
 
 import com.data_arts_studio.web_mvp_back.project.adapter.out.persistence.jpa.ProjectJpaEntity;
 import com.data_arts_studio.web_mvp_back.project.domain.Project;
+import com.data_arts_studio.web_mvp_back.project.domain.ProjectId;
 
 public class ProjectMapper {
+    // Domain Entity -> JPA Entity
     public static ProjectJpaEntity toJpaEntity(Project project) {
-        return new ProjectJpaEntity(project.getId().getId().toString(), project.getName(), project.getPassword(), project.getDescription(), project.getOwnerName(), project.getCreatedAt(), project.getUpdatedAt(), project.getDeletedAt());
+        return new ProjectJpaEntity(
+            project.getId().getId().toString(), 
+            project.getName(), 
+            project.getPassword(), 
+            project.getDescription(), 
+            project.getOwnerName(), 
+            project.getStatus(),
+            project.getCreatedAt(), 
+            project.getUpdatedAt(), 
+            project.getDeletedAt());
+    }
+    // JPA Entity -> Domain Entity
+    public static Project toDomain(ProjectJpaEntity entity) {
+        return new Project(
+            new ProjectId(
+            entity.getId()),
+            entity.getName(),
+            entity.getPassword(),
+            entity.getDescription(),
+            entity.getOwnerName(),
+            entity.getStatus(), 
+            entity.getCreatedAt(),
+            entity.getUpdatedAt(),
+            entity.getDeletedAt()
+        );
     }
 }   
