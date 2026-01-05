fd2bed2212062db2c2cf690cee00b475bbc78ec8
teauk <kto031124@gmail.com>
2026-01-05 12:45:33 +0900

    chore: Project JPA 엔티티에 status 필드 추가와 @Getter추가

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/adapter/out/persistence/jpa/ProjectJpaEntity.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/adapter/out/persistence/jpa/ProjectJpaEntity.java
@@ -1,12 +1,18 @@
 package com.data_arts_studio.web_mvp_back.project.adapter.out.persistence.jpa;
 
 import java.time.LocalDateTime;
+
+import com.data_arts_studio.web_mvp_back.project.domain.ProjectStatus;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
 // JPA 엔티티 클래스
+@Getter
 @Entity
 @Table(name = "projects")
 public class ProjectJpaEntity {
@@ -24,6 +30,9 @@ public class ProjectJpaEntity {
 
     @Column(name = "owner_name")
     private String ownerName;
+
+    @Column(nullable = false)
+    private ProjectStatus status;
     
     @Column(name = "created_at")
     private LocalDateTime createdAt;
@@ -37,40 +46,16 @@ public class ProjectJpaEntity {
 
     protected ProjectJpaEntity() {}
 
-    public ProjectJpaEntity(String id, String name, String password, String description, String ownerName, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
+    public ProjectJpaEntity(String id, String name, String password, String description, String ownerName, ProjectStatus status, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
         this.id = id;
         this.name = name;
         this.password = password;
         this.description = description;
         this.ownerName = ownerName;
+        this.status = status;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
         this.deletedAt = deletedAt;
-    }
-    
-    public String getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public String getOwnerName() {
-        return ownerName;   
-    }
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-    public LocalDateTime getUppdatedAt() {
-        return updatedAt;
-    }
-    public LocalDateTime getDeletedAt() {
-        return deletedAt;
     }
 }
 
