b94197bbdeace795bc9a1d68c8f884d2ab78ec37
teauk <kto031124@gmail.com>
2026-01-09 11:19:03 +0900

    chore: 포트 인터페이스에 주석 추가

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_suite/application/port/out/CheckTestSuiteNamePort.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_suite/application/port/out/CheckTestSuiteNamePort.java
@@ -1,5 +1,7 @@
 package com.data_arts_studio.web_mvp_back.test_suite.application.port.out;
 
+
+// 테스트스위트 이름의 중복 여부를 확인하는 출력 포트
 public interface CheckTestSuiteNamePort {
      boolean isTestSuiteNameDuplicated(String projectId, String name);
 }
--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_suite/application/port/out/SaveTestSuitePort.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_suite/application/port/out/SaveTestSuitePort.java
@@ -2,6 +2,8 @@ package com.data_arts_studio.web_mvp_back.test_suite.application.port.out;
 
 import com.data_arts_studio.web_mvp_back.test_suite.domain.TestSuite;
 
+
+// // 테스트스위트 저장을 위한 출력 포트
 public interface SaveTestSuitePort {
     void save(TestSuite testSuite);
 }
