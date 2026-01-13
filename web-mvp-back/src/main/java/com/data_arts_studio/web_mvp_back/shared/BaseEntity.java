e1479e9524b5f00f9057092b17cfe85a856ad771
teauk <kto031124@gmail.com>
2026-01-13 16:02:00 +0900

    chore: 공통으로 가져야할 엔티티에 삭제 필드에서 아카이브 필드로 변경

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/shared/BaseEntity.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/shared/BaseEntity.java
@@ -3,35 +3,34 @@ package com.data_arts_studio.web_mvp_back.shared;
 import java.time.LocalDateTime;
 
 public abstract class BaseEntity {
-    // 모든 엔티티가 공통으로 가져야할 생성과 수정 타임스탬프 필드
-    protected LocalDateTime createdAt;
-    protected LocalDateTime updatedAt;
-    // 삭제 타임스탬프 필드 
-    protected LocalDateTime deletedAt;
+    // 모든 엔티티가 공통으로 가져야할 필드
+    protected LocalDateTime createdAt; // 생성일시
+    protected LocalDateTime updatedAt; // 수정일시
+    protected LocalDateTime archivedAt; // 아카이브 일시 
     
     public BaseEntity() {
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
-        this.deletedAt = null; // 삭제되지 않으면 null 처리 
+        this.archivedAt = null; // 삭제되지 않으면 null 처리 
     }
 
-    public void restoreAuditFields(LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
+    public void restoreAuditFields(LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime archivedAt) {
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
-        this.deletedAt = deletedAt;
+        this.archivedAt = archivedAt;
     }
 
     public void markUpdated() {
         this.updatedAt = LocalDateTime.now();
     }
 
-    public void markDeleted() {
-        this.deletedAt = LocalDateTime.now();
+    public void markArchived() {
+        this.archivedAt = LocalDateTime.now();
     }
 
     // 삭제 복원 메서드
     public void restore() {
-        this.deletedAt = null;
+        this.archivedAt = null;
         this.markUpdated();
     }
 
@@ -44,8 +43,8 @@ public abstract class BaseEntity {
         return updatedAt;
     }
 
-    public LocalDateTime getDeletedAt() {
-        return deletedAt;
+    public LocalDateTime getArchivedAt() {
+        return archivedAt;
     }
 
 }
