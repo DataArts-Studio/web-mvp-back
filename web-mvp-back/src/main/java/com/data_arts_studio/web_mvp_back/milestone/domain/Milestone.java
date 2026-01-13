eb77d2a814d7582781b72db3a271fdf4a2512ecd
teauk <kto031124@gmail.com>
2026-01-13 18:21:09 +0900

    refactor: Milestone 도메인 객체를 정책에 맞게 수정

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/milestone/domain/Milestone.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/milestone/domain/Milestone.java
@@ -1,58 +1,67 @@
 package com.data_arts_studio.web_mvp_back.milestone.domain;
 
+import java.time.OffsetDateTime;
+
 import com.data_arts_studio.web_mvp_back.project.domain.ProjectId;
 import com.data_arts_studio.web_mvp_back.shared.BaseEntity;
 
-
+// 마일스톤 도메인 모델 스켈레톤
 public class Milestone extends BaseEntity {
-    // 마일스톤 도메인 모델 스켈레톤
     private final MilestoneId id; // 마일스톤 식별자
     private final ProjectId projectId; // 연관된 식별자 (Project)
+
     private String name; // 마일스톤 이름
     private String description; // 마일스톤 설명
-    private MilestoneStatus status; // 마일스톤 상태
 
-    public Milestone(MilestoneId id, ProjectId projectId, String name, String description, MilestoneStatus status) {
+    private OffsetDateTime startDate; // 마일스톤 시작일
+    private OffsetDateTime endDate; // 마일스톤 종료일
+    private MilestoneProgressStatus milestoneProgressStatus; // 마일스톤 상태
+
+    public Milestone(MilestoneId id, ProjectId projectId, String name, String description, OffsetDateTime startDate, OffsetDateTime endDate, MilestoneProgressStatus milestoneProgressStatus) {
         super();
         this.id = id;
         this.projectId = projectId;
         this.name = name;
         this.description = description;
-        this.status = status;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.milestoneProgressStatus = milestoneProgressStatus;
     }
-    
+    // Getter 메서드
     public MilestoneId getId() {
         return id;
     }
-    
     public ProjectId getProjectId() {
         return projectId;
     }
-    
     public String getName() {
         return name;
     }
-
     public String getDescription() {
         return description;
     }
-
-    public MilestoneStatus getStatus() {
-        return status;
+    public OffsetDateTime getStartDate() {
+        return startDate;
     }
-
+    public OffsetDateTime getEndDate() {
+        return endDate;
+    }
+    public MilestoneProgressStatus getStatus() {
+        return milestoneProgressStatus;
+    }
+    // 마일스톤 이름 변경
     public void rename(String newName) {
         this.name = newName;
         markUpdated();
     }
-
+    // 마일스톤 설명 변경
     public void updateDescription(String newDescription) {
         this.description = newDescription;
         markUpdated();
     }
-
-    public void updateStatus(MilestoneStatus newStatus) {
-        this.status = newStatus;
+    // 마일스톤 상태 변경
+    public void updateStatus(MilestoneProgressStatus newStatus) {
+        this.milestoneProgressStatus = newStatus;
         markUpdated();
     }
 }
