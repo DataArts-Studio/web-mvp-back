a0275b4f2683ae555892dc6f6a590a3e869d0678
teauk <kto031124@gmail.com>
2026-01-13 16:33:01 +0900

    chore: lifecycleStatus 필드 추가와 데이터 타입 변경
    -  LocalDateTime -> timestamptz 불일치 문제를 OffsetDateTime 타입으로 변경하게 일치되게 변경
    
    - lifecycle_status 필드 추가

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/shared/BaseEntity.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/shared/BaseEntity.java
@@ -1,50 +1,55 @@
 package com.data_arts_studio.web_mvp_back.shared;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 public abstract class BaseEntity {
     // 모든 엔티티가 공통으로 가져야할 필드
-    protected LocalDateTime createdAt; // 생성일시
-    protected LocalDateTime updatedAt; // 수정일시
-    protected LocalDateTime archivedAt; // 아카이브 일시 
+    protected OffsetDateTime createdAt; // 생성일시
+    protected OffsetDateTime updatedAt; // 수정일시
+    protected OffsetDateTime archivedAt; // 아카이브 일시 
+    protected LifecycleStatus lifecycleStatus; // 라이프사이클 상태
     
-    public BaseEntity() {
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-        this.archivedAt = null; // 삭제되지 않으면 null 처리 
+    protected BaseEntity() {
+        this.createdAt = OffsetDateTime.now();
+        this.updatedAt = OffsetDateTime.now();
+        this.lifecycleStatus = LifecycleStatus.ACTIVE; // 기본 상태 ACTIVE
     }
 
-    public void restoreAuditFields(LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime archivedAt) {
+    // 감사 필드 복원 메서드
+    public void restoreAuditFields(OffsetDateTime createdAt, OffsetDateTime updatedAt, OffsetDateTime archivedAt) {
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
         this.archivedAt = archivedAt;
     }
-
-    public void markUpdated() {
-        this.updatedAt = LocalDateTime.now();
+    // 수정 일시 갱신 메서드
+    protected void markUpdated() {
+        this.updatedAt = OffsetDateTime.now();
     }
-
+    // 아카이브 메서드
     public void markArchived() {
-        this.archivedAt = LocalDateTime.now();
+        this.lifecycleStatus = LifecycleStatus.ARCHIVED;
+        this.archivedAt = OffsetDateTime.now();
+        this.markUpdated();
     }
-
     // 삭제 복원 메서드
     public void restore() {
+        this.lifecycleStatus = LifecycleStatus.ACTIVE;
         this.archivedAt = null;
         this.markUpdated();
     }
 
-
-    public LocalDateTime getCreatedAt() {
+    //  Getter 메서드
+    public OffsetDateTime getCreatedAt() {
         return createdAt;
     }
-    
-    public LocalDateTime getUpdatedAt() {
+    public OffsetDateTime getUpdatedAt() {
         return updatedAt;
     }
-
-    public LocalDateTime getArchivedAt() {
+    public OffsetDateTime getArchivedAt() {
         return archivedAt;
+    }
+    public LifecycleStatus getLifecycleStatus() {
+        return lifecycleStatus;
     }
 
 }
