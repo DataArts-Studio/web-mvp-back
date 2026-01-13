caab4c10fcd39afe53f5939d3b98a8a8f14cd48f
teauk <kto031124@gmail.com>
2026-01-13 22:46:30 +0900

    refactor: Test Run 상태 재정의

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_run/domain/TestRunStatus.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_run/domain/TestRunStatus.java
@@ -1,7 +1,9 @@
 package com.data_arts_studio.web_mvp_back.test_run.domain;
 
+// 테스트 런의 상태를 나타내는 enum 클래스
 public enum TestRunStatus {
-    PASS,
-    FAIL,
-    BLOCKED
+    PENDING, // 대기 중
+    RUNNING, // 실행 중
+    COMPLETED, // 완료됨
+    ABORTED // 중단됨
 }
