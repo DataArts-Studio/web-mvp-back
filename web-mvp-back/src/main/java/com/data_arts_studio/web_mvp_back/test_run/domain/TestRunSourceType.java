e91d12c891ed5e6aaed04d7878385032ce328609
teauk <kto031124@gmail.com>
2026-01-13 22:45:58 +0900

    feat: Test Run의 소스 유형을 나타내는 enum 클래스 추가

---
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_run/domain/TestRunSourceType.java
@@ -0,0 +1,8 @@
+package com.data_arts_studio.web_mvp_back.test_run.domain;
+
+// 테스트 런의 소스 유형을 나타내는 enum 클래스
+public enum TestRunSourceType {
+    MANUAL, // 수동
+    AUTOMATION, // 자동
+    CI // 통합
+}
