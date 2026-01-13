0f492cf8df9fe43bd4a8f3667d4e36c009bc9e78
teauk <kto031124@gmail.com>
2026-01-13 18:21:32 +0900

    refactor: 마일스톤의 상태를 나타내는 enum 클래스를 정책에 맞게 재정의

---
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/milestone/domain/MilestoneProgressStatus.java
@@ -0,0 +1,8 @@
+package com.data_arts_studio.web_mvp_back.milestone.domain;
+
+// 마일스톤의 진행 상태를 나태내는 enum 클래스
+public enum MilestoneProgressStatus {
+    PLANNED, // 계획됨
+    IN_PROGRESS, // 진행 중
+    DONE // 완료됨
+}
--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/milestone/domain/MilestoneStatus.java
+++
@@ -1,6 +0,0 @@
-package com.data_arts_studio.web_mvp_back.milestone.domain;
-
-public enum MilestoneStatus {
-    ACTIVE,
-    ARCHIVED
-}
