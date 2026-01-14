961e33a9d223231bd3e1d705203fa5d7368074ef
teauk <kto031124@gmail.com>
2026-01-14 16:23:01 +0900

    refactor: 정책 변경에 맞게 프로젝트 생성 결과 객체 변경
    - Project sulg 필드 추가
    - 생성 날짜를 나타내는 LocalDateTime -> OffsetDateTime 타입으로 변경
    - slug를 매개로 URL을 생성하는 메서드 추가

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/application/service/ProjectResult.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/application/service/ProjectResult.java
@@ -1,12 +1,18 @@
 package com.data_arts_studio.web_mvp_back.project.application.service;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
-// 서비스에서 반환할 DTO 클래스
-public record  ProjectResult (
+// 서비스에서 반환할 프로젝트 생성 결과 클래스
+// URL 생성 메서드 포함
+public record ProjectResult (
         String projectId,
         String name,
+        String slug,
         String description,
         String ownerName,
-        LocalDateTime createdAt){
+        OffsetDateTime createdAt){
+
+        public String getProjectUrl() {
+                return "/projects/" + slug;
+        }
 }
