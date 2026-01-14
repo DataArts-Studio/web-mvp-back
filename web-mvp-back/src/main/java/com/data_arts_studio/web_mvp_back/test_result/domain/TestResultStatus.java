0f4ce6761167cf6138630f213e5b77829c41a524
teauk <kto031124@gmail.com>
2026-01-14 15:22:56 +0900

    feat: TestResult의 상태를 나타내는 enum 클래스 정의

---
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_result/domain/TestResultStatus.java
@@ -0,0 +1,10 @@
+package com.data_arts_studio.web_mvp_back.test_result.domain;
+
+// 테스트 결과의 상태를 나타내는 enum 클래스
+public enum TestResultStatus {
+    PENDING,
+    PASS, 
+    FAIL, 
+    BLOCKED, 
+    SKIPPED   
+}
