19df7add642d839018268c09464fc5bf3fbfabc6
teauk <kto031124@gmail.com>
2026-02-03 15:32:56 +0900

    feat: ProjectJpaRepository 존재 여부 검증 메서드 확장 및 정책 변경
    - existsBySlug 추가로 슬러그 중복 검증 지원
    - 삭제 기준을 deletedAt -> archivedAt 기준으로 변경
    - existsByIdAndDeletedAtIsNull -> existsByArchivedAtIsNullAndId 변경

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/adapter/out/persistence/jpa/ProjectJpaRepository.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/adapter/out/persistence/jpa/ProjectJpaRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProjectJpaRepository extends JpaRepository<ProjectJpaEntity, String> {
     // 프로젝트 이름으로 존재 여부 확인
     boolean existsByName(String name);
-    
-    // 삭제되지 않은 프로젝트 ID로 존재 여부 확인
-    boolean existsByIdAndDeletedAtIsNull(String projectId);
+    // 프로젝트 슬로거로 존재 여부 확인 
+    boolean existsBySlug(String slug);
+    // 아카이브 되지 않은 프로젝트 ID로 존재 여부 확인
+    boolean existsByArchivedAtIsNullAndId(String projectId);
+
 
 }
