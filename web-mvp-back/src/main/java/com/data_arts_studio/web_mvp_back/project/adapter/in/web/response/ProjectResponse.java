f185c01c5dfc2192be4efac0685801c25eed3aa7
teauk <kto031124@gmail.com>
2026-02-03 11:20:53 +0900

    chore: 프로젝트 생성 응답 DTO 객체 타입 변경 - LocalDateTime -> OffsetDateTime

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/adapter/in/web/response/ProjectResponse.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/adapter/in/web/response/ProjectResponse.java
@@ -1,8 +1,7 @@
 package com.data_arts_studio.web_mvp_back.project.adapter.in.web.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 public record ProjectResponse(
     String id,
@@ -10,7 +9,7 @@ public record ProjectResponse(
     String description,
     String ownerName,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    LocalDateTime createdAt
+    OffsetDateTime createdAt
 ) {
     
 }
