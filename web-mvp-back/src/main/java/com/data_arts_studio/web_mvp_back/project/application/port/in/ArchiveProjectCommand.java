6fd17b7ca908670f84cbd74f0036347ea23e539a
teauk <kto031124@gmail.com>
2026-02-03 14:30:05 +0900

    chore: 프로젝트 아카이브 시 필요 없는 커맨드 객체의 필드 삭제 및 주석 추가

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/application/port/in/ArchiveProjectCommand.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/application/port/in/ArchiveProjectCommand.java
@@ -1,10 +1,7 @@
 package com.data_arts_studio.web_mvp_back.project.application.port.in;
 
-import java.time.LocalDateTime;
-
-// 프로젝트 아카이브 요청 시에 필요한 커맨드 객체 (입력)
+// 프로젝트 아카이브 요청 시에 필요한 커맨드 객체 
 public record ArchiveProjectCommand(
     String projectId,
-    String confirmName,
-    LocalDateTime archivedAt
+    String confirmName // 사용자가 확인을 위해 입력할 프로젝트 이름
 ) {}
