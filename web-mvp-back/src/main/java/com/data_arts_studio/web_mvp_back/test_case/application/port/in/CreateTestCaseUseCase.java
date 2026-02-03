e21bc762acd5e8264fb27d98898e863daa8ce6f1
teauk <kto031124@gmail.com>
2026-02-03 12:50:41 +0900

    chore: 아직 구현 안 된 test_case 모듈을 임시 제거 - 구조 정리 목적

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_case/application/port/in/CreateTestCaseUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.data_arts_studio.web_mvp_back.test_case.application.port.in;
-                                                                    
-import com.data_arts_studio.web_mvp_back.test_case.application.service.TestResult;
-
-// 테스트 케이스 생성 유스케이스
-public interface CreateTestCaseUseCase {
-    TestResult createTestCase(CreateTestCaseCommand command);
-}
