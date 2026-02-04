a712bdb41dd87627ec193de3b4d85846a92c1031
teauk <kto031124@gmail.com>
2026-02-04 10:38:03 +0900

    chore: 바뀐 정책에 따라 TestSuite 생성 커맨드에 description 필드 추가

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_suite/application/port/in/CreateTestSuiteCommand.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_suite/application/port/in/CreateTestSuiteCommand.java
@@ -1,11 +1,11 @@
 package com.data_arts_studio.web_mvp_back.test_suite.application.port.in;
 
+import lombok.Builder;
 
+
+@Builder
 public record CreateTestSuiteCommand(
     String projectId,
-    String name
-    // ,String description 추 후 필요 시 추가 가능
-
-) {
-    
-}
+    String name,
+    String description
+) {}
