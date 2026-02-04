bebead09e9ee4b10d3412c9128dda8639f3c06cb
teauk <kto031124@gmail.com>
2026-02-04 11:48:34 +0900

    chore: 주석 변경

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_suite/application/service/TestSuiteResult.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_suite/application/service/TestSuiteResult.java
@@ -3,7 +3,7 @@ package com.data_arts_studio.web_mvp_back.test_suite.application.service;
 import java.time.OffsetDateTime;
 
 
-// 테스트 스위트 생상 결과를 나타내는 레코드
+// 테스트 스위트 생성 결과를 나타내는 레코드
 public record TestSuiteResult(
     String id, // TestSuiteId
     String projectId, 
