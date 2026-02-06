b0796cd87af3755c303ecc534786e07162f3e51a
teauk <kto031124@gmail.com>
2026-02-06 16:30:57 +0900

    feat: TestCase 갱신 Use Case 추가

---
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_case/application/port/in/UpdateTestCaseUseCase.java
@@ -0,0 +1,6 @@
+package com.data_arts_studio.web_mvp_back.test_case.application.port.in;
+
+public interface UpdateTestCaseUseCase {
+    void updateTestCase(UpdateTestCaseCommand command);
+    
+}
