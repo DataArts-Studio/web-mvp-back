97cb03141ecc16e71b15274efaf283c924293f01
teauk <kto031124@gmail.com>
2026-02-11 10:29:10 +0900

    refactor(test-case): UpdateTestCaseUseCase 결과 반환하도록 계약 변경
    - updateTestCase 메서드 반환타입 void -> UpdateTestCaseResult
    - 수정 결과를 애플리케이션 계층에서 전달하도록 개선

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_case/application/port/in/UpdateTestCaseUseCase.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_case/application/port/in/UpdateTestCaseUseCase.java
@@ -1,6 +1,8 @@
 package com.data_arts_studio.web_mvp_back.test_case.application.port.in;
 
+import com.data_arts_studio.web_mvp_back.test_case.application.service.UpdateTestCaseResult;
+
 public interface UpdateTestCaseUseCase {
-    void updateTestCase(UpdateTestCaseCommand command);
+    UpdateTestCaseResult updateTestCase(UpdateTestCaseCommand command);
     
 }
