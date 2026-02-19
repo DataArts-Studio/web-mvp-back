8e867456faedef52496d83cfe20b8adb995578e4
teauk <kto031124@gmail.com>
2026-02-19 15:55:18 +0900

    feat(milestone): milestone_test_cases 복합키 엔티티 매핑 추가
    - milestone_test_cases용 복합키 클래스(MilestoneTestCaseJpaId) 추가
    - 조인 엔티티(MilestoneTestCaseJpaEntity) 및 리포지토리 추가
    - milestone-testCase 연결 테이블을 JPA로 조회 가능하도록 구성

---
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/milestone/adapter/out/persistence/jpa/MilestoneTestCaseJpaId.java
@@ -0,0 +1,24 @@
+package com.data_arts_studio.web_mvp_back.milestone.adapter.out.persistence.jpa;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MilestoneTestCaseJpaId implements Serializable {
+    @Column(name = "milestone_id", nullable = false)
+    private String milestoneId;
+
+    @Column(name = "test_case_id", nullable = false)
+    private String testCaseId;
+}
