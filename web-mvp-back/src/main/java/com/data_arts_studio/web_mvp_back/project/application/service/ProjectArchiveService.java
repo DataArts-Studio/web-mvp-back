01623709b5b418ec8cf49fb44e559adfb955d505
teauk <kto031124@gmail.com>
2026-02-24 14:34:55 +0900

    refactor(project):  ArchiveProject Service 에서 SaveProjectPort 제거
    - ProjectArchiveService에서 SaveProjectPort 의존성 제거
    - ArchiveProjectPort 도입으로 아카이브 행위 명확화
    - save(project)에서 archive(project)로 변경
    - 행위 기반 포트 설계 원칙에 맞게 구조 정리
    
    아카이브 유스케이스가 범용 저장 포트에 의존하지 않도록
    의도를 명확하게 분리함.

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/application/service/ProjectArchiveService.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/application/service/ProjectArchiveService.java
@@ -5,8 +5,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.data_arts_studio.web_mvp_back.project.application.port.in.ArchiveProjectCommand;
 import com.data_arts_studio.web_mvp_back.project.application.port.in.ArchiveProjectUseCase;
+import com.data_arts_studio.web_mvp_back.project.application.port.out.ArchiveProjectPort;
 import com.data_arts_studio.web_mvp_back.project.application.port.out.LoadProjectPort;
-import com.data_arts_studio.web_mvp_back.project.application.port.out.SaveProjectPort;
 import com.data_arts_studio.web_mvp_back.project.application.validator.ProjectArchiveValidator;
 import com.data_arts_studio.web_mvp_back.project.application.exception.ProjectBusinessException;
 import com.data_arts_studio.web_mvp_back.project.application.exception.ProjectErrorCode;
@@ -20,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class ProjectArchiveService implements ArchiveProjectUseCase {
     private final LoadProjectPort loadProjectPort;
-    private final SaveProjectPort saveProjectPort;
+    private final ArchiveProjectPort archiveProjectPort;
     private final ProjectArchiveValidator projectArchiveValidator;
 
     @Override
@@ -29,6 +29,6 @@ public class ProjectArchiveService implements ArchiveProjectUseCase {
             .orElseThrow(() -> new ProjectBusinessException(ProjectErrorCode.PROJECT_NOT_FOUND));
         projectArchiveValidator.validate(project, command);
         project.markArchived();
-        saveProjectPort.save(project);
+        archiveProjectPort.archive(project);
     }
 }
