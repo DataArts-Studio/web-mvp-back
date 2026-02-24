c283a0ac35fe2e7396a2d150c90b7f97de566014
teauk <kto031124@gmail.com>
2026-02-24 14:36:45 +0900

    refactor(project): SaveProjectPort를 생성,수정을 포함한 저장용 인터페이스로 분리
    - createProject(Project), updateProject(Project)로 역할 명확화
    - 범용 저장 포트 대신 행위 기반 포트 설계로 구조 개선
    
    프로젝트 생성과 수정 책임을 명확히 분리하여 헥사고날 아키텍처의 의도 표현을 강화함.

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/application/port/out/SaveProjectPort.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/project/application/port/out/SaveProjectPort.java
@@ -2,6 +2,17 @@ package com.data_arts_studio.web_mvp_back.project.application.port.out;
 
 import com.data_arts_studio.web_mvp_back.project.domain.Project;
 
+// 프로젝트 저장 관련 포트 인터페이스
 public interface SaveProjectPort {
-    void save(Project project);
-}
+    /**
+     * 프로젝트 생성
+     * @param project 생성할 프로젝트 도메인 객체
+     */
+    void createProject(Project project);
+
+    /**
+     * 프로젝트 수정
+     * @param project 수정할 프로젝트 도메인 객체
+     */
+    void updateProject(Project project);
+} 
