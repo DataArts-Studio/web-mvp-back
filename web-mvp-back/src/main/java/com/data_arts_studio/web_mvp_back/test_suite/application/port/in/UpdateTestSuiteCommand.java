3392d8831a8268f9fd9d7c1f0fd2ff0cc8711a1e
teauk <kto031124@gmail.com>
2026-03-03 09:52:21 +0900

    chore(test-suite): TestSuite update 커맨드 객체 필드 네이밍 수정

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_suite/application/port/in/UpdateTestSuiteCommand.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_suite/application/port/in/UpdateTestSuiteCommand.java
@@ -4,13 +4,15 @@ import lombok.Builder;
 
 /**
  * 테스트 스위트 수정 커맨드
- * @param testSuiteId 테스트 스위트 ID
- * @param name 테스트 스위트 이름
- * @param description 테스트 스위트 설명
+ * @param id            테스트 스위트 ID
+ * @param projectId     해당 스위트의 프로젝트 ID
+ * @param name          테스트 스위트 이름
+ * @param description   테스트 스위트 설명
  */
 @Builder
 public record UpdateTestSuiteCommand(
-    String testSuiteId,
+    String id,
+    String projectId,
     String name,
     String description
 ) {
