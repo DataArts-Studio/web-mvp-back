5a287e75ea46acbab12a76db1084a612bee07dfd
teauk <kto031124@gmail.com>
2026-03-06 14:34:42 +0900

    feat(test-case): 테스트 케이스 아카이브 유스케이스 계약 정리
    - 아카이브 처리 범위(프로젝트/테스트케이스 식별자) 계약 명확화

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_case/application/port/in/usecase/ArchiveTestCaseUseCase.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_case/application/port/in/usecase/ArchiveTestCaseUseCase.java
@@ -1,9 +1,18 @@
 package com.data_arts_studio.web_mvp_back.test_case.application.port.in.usecase;
 
+import com.data_arts_studio.web_mvp_back.test_case.application.port.in.command.ArchiveTestCaseCommand;
+
+/**
+ * 테스트 케이스 아카이브 처리 인바운드 유스케이스.
+ *
+ * 웹 어댑터 등 입력 계층에서 전달한 아카이브 요청 커맨드를 받아 지정된 테스트 케이스를 아카이브 처리
+ */
 public interface ArchiveTestCaseUseCase {
+
     /**
-     * TestCase를 id로 아카이브 처리
-     * @param testCaseId
+     * 특정 프로젝트 범위의 테스트 케이스를 아카이브 처리
+     *
+     * @param command 아카이브 대상 프로젝트/테스트 케이스 식별자 커맨드
      */
-    void archiveTestCase(String testCaseId);
+    void archiveTestCase(ArchiveTestCaseCommand command);
 }
