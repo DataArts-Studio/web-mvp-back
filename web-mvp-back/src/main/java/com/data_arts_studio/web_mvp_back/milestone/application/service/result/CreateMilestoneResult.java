02cf168acbd48fc44524ab85f671669e7de667b7
teauk <kto031124@gmail.com>
2026-03-19 11:35:05 +0900

    docs(milestone): CreateMilestoneResult 주석 추가

--- web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/milestone/application/service/result/CreateMilestoneResult.java
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/milestone/application/service/result/CreateMilestoneResult.java
@@ -5,7 +5,16 @@ import java.time.OffsetDateTime;
 import lombok.Builder;
 
 /**
- * 마일스톤 생성 결과 
+ * 마일스톤 생성 결과
+ *
+ * @param id 생성된 마일스톤 식별자
+ * @param projectId 프로젝트 식별자
+ * @param name 생성된 마일스톤 이름
+ * @param description 생성된 마일스톤 설명
+ * @param status 생성된 마일스톤 상태
+ * @param startDate 시작 일시
+ * @param endDate 종료 일시
+ * @param createdAt 생성 일시
  */
 @Builder
 public record CreateMilestoneResult(
