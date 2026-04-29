7cca3169410cb3ef5bd4d321639884d99d52f671
teauk <kto031124@gmail.com>
2026-04-29 12:58:11 +0900

    feat(test-run): 테스트 런 마일스톤 연결 JPA 엔티티 추가
    테스트 런 생성 시 선택된 마일스톤 목록을
    별도 연결 테이블에 스냅샷으로 저장하기 위해 JPA 엔티티를 추가함
    
    - test_run_milestones 테이블 매핑 엔티티 추가
    - 테스트 런 ID와 마일스톤 ID를 복합키로 관리
    - 테스트 런과 기준 마일스톤 간 연결 정보 저장 구조 마련
    
    JPA 엔티티를 통해
    복수 마일스톤 기반 테스트 런 생성 결과를 영속성 계층에 저장할 수 있도록 함

---
+++ web-mvp-back/src/main/java/com/data_arts_studio/web_mvp_back/test_run/adapter/out/persistence/jpa/TestRunMilestoneJpaEntity.java
@@ -0,0 +1,21 @@
+package com.data_arts_studio.web_mvp_back.test_run.adapter.out.persistence.jpa;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "test_run_milestones")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class TestRunMilestoneJpaEntity {
+
+    @EmbeddedId
+    private TestRunMilestoneJpaId id;
+
+}
